ab75246e6629501e35e9680befb7ef05423e19db
Small runze <yun_act@163.com>
2023-06-14 17:45:11 +0800

    Add files via upload

--- DeepLysin/Predict.py
+++ DeepLysin/Predict.py
@@ -55,22 +55,19 @@ if __name__ == '__main__':
     Path(os.path.abspath(args.model_path) + '/meta/').mkdir(exist_ok=True,parents=True)
     start_time = time.time()
     clf_feature_order = {
-      "AAE" : ["AAE_ERT"],
-      "AAI" : ["AAI_XGB"],
-      "BPNC" : ["BPNC_ANN"],
-      "CTD" : ["CTD_LR"],
-      "DPC" : ["DPC_ANN","DPC_KNN"],
-      "GTPC" : ["GTPC_ANN","GTPC_XGB"]
+        "AAE" : ["AAE_ERT","AAE_KNN","AAE_XGB"],
+        "CTD" : ["CTD_ERT","CTD_ANN","CTD_XGB"],
+        "DPC" : ["DPC_ERT","DPC_XGB"]
     }
     
     AAE_ERT = joblib.load(os.path.abspath(args.model_path) + '/base/AAE_ERT.m')
-    AAI_XGB = joblib.load(os.path.abspath(args.model_path) + '/base/AAI_XGB.m')
-    BPNC_ANN = joblib.load(os.path.abspath(args.model_path) + '/base/BPNC_ANN.m')
-    CTD_LR = joblib.load(os.path.abspath(args.model_path) + '/base/CTD_LR.m')
-    DPC_ANN = joblib.load(os.path.abspath(args.model_path) + '/base/DPC_ANN.m')
-    DPC_KNN = joblib.load(os.path.abspath(args.model_path) + '/base/DPC_KNN.m')
-    GTPC_ANN = joblib.load(os.path.abspath(args.model_path) + '/base/GTPC_ANN.m')
-    GTPC_XGB = joblib.load(os.path.abspath(args.model_path) + '/base/GTPC_XGB.m')
+    AAE_KNN = joblib.load(os.path.abspath(args.model_path) + '/base/AAE_KNN.m')
+    AAE_XGB = joblib.load(os.path.abspath(args.model_path) + '/base/AAE_XGB.m')
+    CTD_ERT = joblib.load(os.path.abspath(args.model_path) + '/base/CTD_ERT.m')
+    CTD_ANN = joblib.load(os.path.abspath(args.model_path) + '/base/CTD_ANN.m')
+    CTD_XGB = joblib.load(os.path.abspath(args.model_path) + '/base/CTD_XGB.m')
+    DPC_ERT = joblib.load(os.path.abspath(args.model_path) + '/base/DPC_ERT.m')
+    DPC_XGB = joblib.load(os.path.abspath(args.model_path) + '/base/DPC_XGB.m')
     
     if Path(os.path.abspath(args.model_path) + '/meta/Meta.m').exists() is False:
         X_train = pd.read_csv(os.path.abspath(args.model_path) + '/Features/Base_features.csv')
