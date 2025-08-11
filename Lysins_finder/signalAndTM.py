8ec676e8e4243c10ad46a7327912a8deef610a47
Small runze <yun_act@163.com>
2025-08-11 21:57:25 +0800

    Update signalAndTM.py

--- Lysins_finder/signalAndTM.py
+++ Lysins_finder/signalAndTM.py
@@ -615,7 +615,7 @@ def main(file, method, ref = '', cbd = '', ead = '', hmmer_coverage = 80, hmmer_
             w.close()
             
                     
-          elif Args.ref == '':
+          elif ref == '':
             print('aaaaa')
             
             with open('./putative_lysins_info_tmp.txt','w') as w:
@@ -872,4 +872,5 @@ if __name__ == "__main__":
     method = 'rpsblast'
     main(file, method = method, ref = '')
      
+
 
