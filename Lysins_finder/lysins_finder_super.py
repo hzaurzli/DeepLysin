64e2b629cbacda71a1848630c6559d2d50f1c9bf
Small runze <yun_act@163.com>
2025-08-26 20:19:55 +0800

    Update lysins_finder_super.py

--- Lysins_finder/lysins_finder_super.py
+++ Lysins_finder/lysins_finder_super.py
@@ -682,7 +682,7 @@ def Domain_filter_hmmer_withoutRef(Domain_location_dict, ident, coverage, over_l
     for item in Domain_location_dict.items():
         key_data = item[0]
         Domain_list = item[1]
-        Domain_list.sort(key=operator.itemgetter(1))
+        Domain_list.sort(key=operator.itemgetter(2))
         start_initial = 0
         ii_keep = 0
         Domain_filter_list = []
@@ -743,7 +743,7 @@ def Domain_filter_hmmer_withRef(Domain_location_dict, ident, coverage, over_lap)
     for item in Domain_location_dict.items():
         key_data = item[0]
         Domain_list = item[1]
-        Domain_list.sort(key=operator.itemgetter(1))
+        Domain_list.sort(key=operator.itemgetter(2))
         start_initial = 0
         ii_keep = 0
         Domain_filter_list = []
@@ -3035,4 +3035,5 @@ if __name__ == "__main__":
     
         else:
             raise ValueError('Error, please check parameter "--bp"')
+
 
